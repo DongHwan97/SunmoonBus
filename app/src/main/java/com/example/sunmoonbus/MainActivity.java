4a1241c407ba0de97dae9618343c5d250c63ddd2
rosua328 <rosua328@gmail.com>
2020-11-14 22:59:49 +0900

    nfc태그XML, 움직이는 버전(따로 올림, 확인필요)

--- app/src/main/java/com/example/sunmoonbus/MainActivity.java
+++ app/src/main/java/com/example/sunmoonbus/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation_tag);
-        card = findViewById(R.id.imageView);
+        card = (ImageView)findViewById(R.id.imageView);
         Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate);
         card.setAnimation(animation);
 
