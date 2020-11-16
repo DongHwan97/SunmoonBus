ec2297704d48d1631cd312fdd69350470d9be6b8
rosua328 <rosua328@gmail.com>
2020-11-16 22:03:05 +0900

    사람 수동 카운트xml

---
+++ app/src/main/java/com/example/sunmoonbus/CountActivity.java
@@ -0,0 +1,15 @@
+package com.example.sunmoonbus;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CountActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_people);
+
+    }
+}
